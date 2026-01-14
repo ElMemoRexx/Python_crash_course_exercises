1062d380bc678b7159d99706693cc9468fd08559
ElMemoRexx <memorexx03@gmail.com>
2026-01-13 21:39:44 -0600

    Añadimos un ejercicio mas 'If statemen con listas 2'

--- Chapter_5_If_statements/toppings2.py
+++ Chapter_5_If_statements/toppings2.py
@@ -1,7 +1,25 @@
 
-requested_toppings = ['´mushroom', 'green peppers', 'extra cheese']
+"""requested_toppings = ['´mushroom', 'green peppers', 'extra cheese']
 
 for requested_topping in requested_toppings:
     print(f"Adding {requested_topping}.")
 
-print("\Finished making your pizza!")
+print("\nFinished making your pizza!")"""
+
+
+"""requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")"""
+
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
