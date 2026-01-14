6a7fce7570a7c60a2c02ef9f71b5fcdc5bc00b7c
ElMemoRexx <memorexx03@gmail.com>
2026-01-13 20:54:07 -0600

    Ejemplo toppings2 - 'If statements con listas'

---
+++ Chapter_5_If_statements/toppings2.py
@@ -0,0 +1,7 @@
+
+requested_toppings = ['´mushroom', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+
+print("\Finished making your pizza!")
