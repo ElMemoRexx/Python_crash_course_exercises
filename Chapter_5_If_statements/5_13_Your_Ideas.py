e9e7b7087792ac17a317062222ee530e00579863
ElMemoRexx <memorexx03@gmail.com>
2026-03-09 22:45:05 -0600

    Recaputulación del capítulo 5

---
+++ Chapter_5_If_statements/5_13_Your_Ideas.py
@@ -0,0 +1,26 @@
+
+# * 5-13. Your Ideas:
+'''At this point, you’re a more capable programmer than you
+were when you started this book. Now that you have a better sense of how
+real-world situations are modeled in programs, you might be thinking of some
+problems you could solve with your own programs. Record any new ideas you
+have about problems you might want to solve as your programming skills continue 
+to improve. Consider games you might want to write, datasets you might
+want to explore, and web applications you’d like to create.'''
+
+# * Sumary
+
+'''In this chapter you learned how to write conditional tests, which always
+evaluate to True or False. You learned to write simple if statements, if-else
+chains, and if-elif-else chains. You began using these structures to identify
+particular conditions you need to test and to know when those conditions
+have been met in your programs. You learned to handle certain items in a
+list differently than all other items while continuing to utilize the efficiency
+of a for loop. You also revisited Python’s style recommendations to ensure
+that your increasingly complex programs are still relatively easy to read and
+understand.
+In Chapter 6 you’ll learn about Python’s dictionaries. A dictionary is
+similar to a list, but it allows you to connect pieces of information. You’ll
+learn how to build dictionaries, loop through them, and use them in combination 
+with lists and if statements. Learning about dictionaries will enable
+you to model an even wider variety of real-world situations.'''
---
+++ Chapter_6_Dictionaries/alien.py
@@ -0,0 +1,7 @@
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+# todo: Continue from page 91, read the introduction from chapter 6, then continue
+# todo; with this example.
