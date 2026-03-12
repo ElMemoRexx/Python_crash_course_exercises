aa4631678c4f1aedf8205ba7a812545bbb5628e5
ElMemoRexx <memorexx03@gmail.com>
2026-03-11 22:50:18 -0600

    Chapter 6 - Dictionaries, Ejercicios con diccionarios

---
+++ Chapter_6_Dictionaries/6_2_Favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_number = {
+    'mark': 14,
+    'daniel': 2,
+    'oscar': 78,
+    'emmanuel': 32,
+    'willie': 3
+}
+
+print(favorite_number['mark'])
---
+++ Chapter_6_Dictionaries/6_3_Glossary.py
@@ -0,0 +1,7 @@
+glossary = {
+    'bool': 'A data type that can only be True or False.',
+    'string': 'A sequence of characters enclosed in quotes.',
+    'tuple': 'An ordered, immutable collection of elements.',
+    'integer': 'A whole number without a decimal point.',
+    'function': 'A reusable block of code that performs a specific task.'
+}
