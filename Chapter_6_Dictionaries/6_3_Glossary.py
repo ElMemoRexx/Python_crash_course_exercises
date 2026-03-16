20ed773ed620beee1f66054f4ccbcb626811a190
ElMemoRexx <memorexx03@gmail.com>
2026-03-16 10:30:13 -0600

    6_3_Glossary - Checkpoint

--- Chapter_6_Dictionaries/6_3_Glossary.py
+++ Chapter_6_Dictionaries/6_3_Glossary.py
@@ -5,3 +5,6 @@ glossary = {
     'integer': 'A whole number without a decimal point.',
     'function': 'A reusable block of code that performs a specific task.'
 }
+
+
+# todo: continue from page 99, "looping Through All Key-Value Pairs"
