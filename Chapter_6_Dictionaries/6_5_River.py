184605569bc0b45a23f7805eb8b89517435b392f
ElMemoRexx <memorexx03@gmail.com>
2026-03-16 12:40:40 -0600

    6_5 Rivers - Ejercicio práctico

---
+++ Chapter_6_Dictionaries/6_5_River.py
@@ -0,0 +1,23 @@
+
+# * 6-5. Rivers: Make a dictionary containing three major rivers and the country
+# * each river runs through. One key-value pair might be 'nile': 'egypt'.
+'''
+• Use a loop to print a sentence about each river, such as The Nile runs
+through Egypt.
+• Use a loop to print the name of each river included in the dictionary.
+• Use a loop to print the name of each country included in the dictionary.'''
+
+rivers = {
+    'nile': 'egypt',
+    'rio bravo': 'mexico',
+    'amazone': 'brazil',
+}
+
+for key, value in rivers.items():
+    print(f"The {key.title()} runs through {value.title()}.")
+
+for river in rivers.keys():
+    print(f"\n{river.title()}")
+
+for river in rivers.values():
+    print(f"\n{river.title()}")
