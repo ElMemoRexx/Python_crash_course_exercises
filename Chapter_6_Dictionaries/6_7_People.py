67e999f23146ad6b631535eda7eadcadffa078ee
ElMemoRexx <memorexx03@gmail.com>
2026-04-05 14:22:26 -0600

    Example of Nesting a list inside a dictionary

--- Chapter_6_Dictionaries/6_7_People.py
+++ Chapter_6_Dictionaries/6_7_People.py
@@ -1,5 +1,6 @@
 
 # * 6-7. People:
 """ Start with the program you wrote for Exercise 6-1 (page 98). Make
-two new dictionaries representing different people, and store all three dictionaries in a list called people. Loop through your list of people. As you loop through
-the list, print everything you know about each person"""
+two new dictionaries representing different people, and store all three 
+dictio-naries in a list called people. Loop through your list of people. As you 
+loop through the list, print everything you know about each person"""
