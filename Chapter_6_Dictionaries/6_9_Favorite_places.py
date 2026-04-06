f434093ee6584ab0c8877525fd3272b956a9f10b
ElMemoRexx <memorexx03@gmail.com>
2026-04-05 21:32:55 -0600

    6-9 Favorite places - Ejercicios con diccionarios y listas

---
+++ Chapter_6_Dictionaries/6_9_Favorite_places.py
@@ -0,0 +1,18 @@
+
+# * 6-9. Favorite Places:
+"""Make a dictionary called favorite_places. Think of three
+names to use as keys in the dictionary, and store one to three favorite places for
+each person. To make this exercise a bit more interesting, ask some friends to
+name a few of their favorite places. Loop through the dictionary, and print each
+person’s name and their favorite places."""
+
+favorite_places = {
+    'willie': ['cancun', 'tulum'],
+    'mayte': ['paris', 'spain'],
+    'matt': ['huayacocotla', 'hidalgo'],
+}
+
+for name, places in favorite_places.items():
+    print(f"\n{name.title()}'s favorite places are:")
+    for place in places:
+        print(f"- {place.title()}")
