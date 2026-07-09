9c62eef2e3eddda75cc141d343b92b5e5f0f6650
ElMemoRexx <memorexx03@gmail.com>
2026-07-08 23:24:31 -0600

    6-10 Favorite Numbers

---
+++ Chapter_6_Dictionaries/6_10_Favorite_Numbers.py
@@ -0,0 +1,21 @@
+
+# * 6-10. Favorite Numbers:
+'''
+Modify your program from Exercise 6-2 (page 98) so
+each person can have more than one favorite number. Then print each person’s
+name along with their favorite numbers.
+'''
+
+favorite_number = {
+    'mark': [14, 20],
+    'daniel': [2, 3],
+    'oscar': [78, 6],
+    'emmanuel': [32, 4, 7],
+    'willie': [3, 1]
+}
+
+
+for name, numbers in favorite_number.items():
+    print(f"\n{name.title()}'s favorite numbers are: ")
+    for number in numbers:
+        print(f"\t{number}")
